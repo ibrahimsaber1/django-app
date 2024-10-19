64bfbc9e31167ccea7b17db67009dc43ffc040bf
IBRAHIM SABER <ibrahimsaber622@gmail.com>
2024-10-19 21:44:34 +0300

    edit: the templests

--- src/pages/views.py
+++ src/pages/views.py
@@ -12,8 +12,12 @@ def contact_page(request):
     return render(request , 'contacr.html',{})
 
 def about_page(request):
-    
-    return render(request , 'about.html',{})
+    my_context = {
+        "title": "our number",
+        "num":"01023456789",
+        "note": "bro dont call us"
+    }
+    return render(request , 'about.html',my_context)
 
 def social_page(request):
     
